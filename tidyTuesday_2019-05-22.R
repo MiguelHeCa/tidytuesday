4997a0d94b792a440c9379622b1339acbd826344
Miguel Hernandez-Cabrera <jose.miguel.heca@gmail.com>
2019-05-23 01:04:48 -0500

    Starting Tidy Tuesday challenge

---
+++ tidyTuesday_2019-05-22.R
@@ -0,0 +1,111 @@
+library(tidyverse)
+library(cowplot)
+library(ggrepel)
+library(gapminder)
+library(janitor)
+library(countrycode)
+
+# Download data from TidyTuesday's repo
+coast_vs_waste <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-05-21/coastal-population-vs-mismanaged-plastic.csv")
+
+mismanaged_vs_gdp <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-05-21/per-capita-mismanaged-plastic-waste-vs-gdp-per-capita.csv")
+
+waste_vs_gdp <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-05-21/per-capita-plastic-waste-vs-gdp-per-capita.csv")
+
+# Check what years have any kind of data
+map(list(coast_vs_waste, mismanaged_vs_gdp, waste_vs_gdp),
+    ~ .x %>% drop_na() %>% distinct(Year))
+# Turns out we only have 2010
+
+# Save old names for future texts
+old_column_names <- map(list(coast_vs_waste, mismanaged_vs_gdp, waste_vs_gdp),
+                        ~ colnames(.x))
+names(old_column_names) <- ls(pattern = "_vs_")
+
+# Check if gdp columns are the same
+m_gdp <- mismanaged_vs_gdp %>% select(gdp = 5) %>% drop_na()
+
+w_gdp <- waste_vs_gdp %>% select(gdp = 5) %>% drop_na()
+
+setequal(m_gdp, w_gdp)
+# indeed they are
+
+# Merge plastic and mismanagement waste, adding continents
+plastic_waste <- waste_vs_gdp %>% 
+  left_join(mismanaged_vs_gdp) %>% 
+  left_join(coast_vs_waste) %>% 
+  drop_na() %>% 
+  clean_names() %>% 
+  mutate(continent = countrycode(code, "iso3c", "continent")) %>% 
+  select(entity,
+         continent,
+         gdp_pc_ppp = ends_with("international"),
+         m_pw = ends_with("tonnes"),
+         pw_pc = matches("capita_pl"),
+         m_pw_pc = matches("capita_mi")) %>% 
+  mutate(annual_waste = pw_pc * 365 * 1000 / 10^6,
+         waste_ratio = m_pw_pc / pw_pc * 100)
+
+
+# plot --------------------------------------------------------------------
+
+
+# Facets continents
+plot.plastic <- ggplot(plastic_waste,
+             aes(x = gdp_pc_ppp, y = pw_pc, size = m_pw)) +
+  geom_point(alpha = 0.7) +
+  geom_smooth(method = "lm", alpha = 0.2) +
+  scale_x_log10(labels = scales::comma) +
+  scale_y_log10() +
+  facet_grid(. ~ continent) +
+  background_grid(major = "xy", minor = "none") +
+  theme(legend.position = "none") 
+#plot.plastic
+
+plot.mismanage <- ggplot(plastic_waste,
+             aes(x = gdp_pc_ppp, y = m_pw_pc, size = m_pw)) +
+  geom_point(alpha = 0.7) +
+  geom_smooth(method = "loess", alpha = 0.2) +
+  scale_x_log10(labels = scales::comma) +
+  scale_y_log10() +
+  facet_grid(. ~ continent) +
+  background_grid(major = "xy", minor = "none") +
+  theme(legend.position = "none") 
+#plot.mismanage
+
+plot_grid(plot.plastic,
+          plot.mismanage, nrow = 2, align = "v")
+
+
+# test code ---------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
