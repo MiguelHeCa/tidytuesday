b37d5357ad7449a9f1f2c20e777f2377e0f68610
Miguel Hernandez-Cabrera <jose.miguel.heca@gmail.com>
2019-06-12 11:23:31 -0500

    Fixing labels and colors

--- 2019-06-11/Meteorites.R
+++ 2019-06-11/Meteorites.R
@@ -15,7 +15,7 @@ world <- map_data("world")
 
 meteo <- meteorites %>%
   drop_na() %>% 
-  filter(mass > 0, year > 1500) %>% 
+  filter(mass > 0, year > 1300) %>% 
   mutate(year = if_else(name == "Northwest Africa 7701", 2010, year),
          year = as.integer(year),
          calc_mass = if_else(mass > 10000, 10000, mass))
@@ -23,10 +23,9 @@ meteo <- meteorites %>%
 fallen <- meteo %>% filter(fall == "Fell")
 found <- meteo %>% filter(fall == "Found")
 
+# Create static plot ------------------------------------------------------
 
-# trial -------------------------------------------------------------------
-
-ggplot() +
+map <- ggplot() +
   geom_polygon(
     data = world,
     aes(x = long, y = lat, group = group),
@@ -81,81 +80,22 @@ ggplot() +
     )
   ) +
   scale_size(guide = "none") +
-  annotate(
-    "text",
-    x = -134,
-    y = -30,
-    family = "B612 Mono",
-    label = "Year: {frame_time}",
-    size = 7,
-    color = "gray50",
-    fontface = "bold"
-  ) +
   theme_void() +
   theme(
     legend.position = c(0.15, 0.1),
     plot.title = element_text(hjust = 0.5),
+    plot.subtitle = element_text(hjust = 0.5, color = "gray50"), 
     plot.caption = element_text(hjust = 0.5),
     text = element_text(family = "B612 Mono")
   ) +
   labs(
     color = "Fallen mass in kilograms",
     fill = "Found mass in kilograms",
-    title = "Fallen and found meteorites on Earth",
+    title = "Meteorites on Earth",
+    subtitle = "Year: {frame_time}",
     caption = "Source: NASA"
   ) +
   coord_map("mollweide", orientation = c(90, 0, 0))
-
-
-# Create static plot ------------------------------------------------------
-
-map <- ggplot() + 
-  geom_polygon(data = world,
-               aes(x = long, y = lat, group = group),
-               fill = "#e6e6e9",
-               size = 0.1) +
-  geom_point(data = fallen,
-             aes(x = long, y = lat, size = calc_mass, color = calc_mass),
-             alpha = 0.5) +
-  scale_color_distiller(palette = "Reds",
-                        direction = 1,
-                        labels = c("1", "2,500", "5,000", "7,500", "10,000+"),
-                        guide = guide_colorbar(
-                          direction = "horizontal",
-                          barheight = unit(2, units = "mm"),
-                          barwidth = unit(50, units = "mm"),
-                          title.position = "top",
-                          title.hjust = 0.5,
-                          label.hjust = 0.5
-                        )) +
-  geom_point(data = found,
-             aes(x = long, y = lat, size = calc_mass, fill = calc_mass),
-             shape = 21,
-             alpha = 0.5) +
-  scale_fill_distiller(palette = "Blues",
-                       direction = 1,
-                       labels = c("1", "2,500", "5,000", "7,500", "10,000+"),
-                       guide = guide_colorbar(
-                         direction = "horizontal",
-                         barheight = unit(2, units = "mm"),
-                         barwidth = unit(50, units = "mm"),
-                         title.position = "top",
-                         title.hjust = 0.5,
-                         label.hjust = 0.5
-                       )) +
-  scale_size(guide = "none") +
-  labs(color = "Fallen mass in grams",
-       fill = "Found mass in grams",
-       title = "Meteorites on Earth",
-       subtitle = "Year: {frame_time}",
-       caption = "Source: NASA") +
-  theme_void() +
-  theme(legend.position = "bottom",
-        panel.background = element_rect(fill = "#000019"),
-        plot.title = element_text(hjust = 0.5, family = "Open Sans"),
-        plot.subtitle = element_text(hjust = 0.5, family = "Open Sans")) +
-  coord_map("mollweide", orientation = c(90, 0, 0))
-
 
 # Create GIF --------------------------------------------------------------
 
@@ -167,6 +107,6 @@ map_gif <- map +
   enter_grow() +
   exit_fade()
 
-animate(map_gif, nframes = 160, duration = 40, height = 1000, width = 1000)
+animate(map_gif, nframes = 160, duration = 40, height = 768, width = 1024)
 
 anim_save("2019-06-11/Meteorites.gif")
