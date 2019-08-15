f137b3e107d5ff9ef94da3abce1c40f451fa5a00
Miguel Hernandez-Cabrera <jose.miguel.heca@gmail.com>
2019-08-15 03:12:10 -0500

    Improving size and resolution

--- 2019-08-14/emperors.R
+++ 2019-08-14/emperors.R
@@ -2,6 +2,11 @@
 
 # Roman Emperors
 
+# References --------------------------------------------------------------
+
+# Inspired in https://www.pmassicotte.com/post/tidytuesday-2019-08-13/
+# Thanks to https://github.com/rfordatascience/tidytuesday
+
 # Setup -------------------------------------------------------------------
 
 library(tidyverse)
@@ -54,6 +59,8 @@ index <- emperors %>%
          ymax = max + .5) %>% 
   arrange(min)
 
+ann_text_size = 5
+
 ggplot(
   imperatorum,
   aes(
@@ -63,11 +70,11 @@ ggplot(
     yend = fct_reorder(NOMINE, index)
   )
 ) +
-  geom_segment(size = 2, lineend = "round", alpha = .2) +
+  geom_segment(size = 4, lineend = "round", alpha = .2) +
   geom_segment(
     aes(x = PRIMO_REGNI, xend = FINE_REGNABUNT),
     color = "#073642",
-    size = 1,
+    size = 2,
     lineend = "round"
   ) +
   annotate(
@@ -85,7 +92,8 @@ ggplot(
     y = index$middle[1],
     label = index$dynasty[1],
     family = "Inknut Antiqua",
-    hjust = 1
+    hjust = 1,
+    size = ann_text_size
   ) +
   annotate(
     "rect",
@@ -102,7 +110,8 @@ ggplot(
     y = index$middle[2],
     label = index$dynasty[2],
     family = "Inknut Antiqua",
-    hjust = 1
+    hjust = 1,
+    size = ann_text_size
   ) +
   annotate(
     "rect",
@@ -119,7 +128,8 @@ ggplot(
     y = index$middle[3],
     label = index$dynasty[3],
     family = "Inknut Antiqua",
-    hjust = 1
+    hjust = 1,
+    size = ann_text_size
   ) +
   annotate(
     "rect",
@@ -136,7 +146,8 @@ ggplot(
     y = index$middle[4],
     label = index$dynasty[4],
     family = "Inknut Antiqua",
-    hjust = 1
+    hjust = 1,
+    size = ann_text_size
   ) +
   annotate(
     "rect",
@@ -153,7 +164,8 @@ ggplot(
     y = index$middle[5],
     label = index$dynasty[5],
     family = "Inknut Antiqua",
-    hjust = 1
+    hjust = 1,
+    size = ann_text_size
   ) +
   annotate(
     "rect",
@@ -170,7 +182,8 @@ ggplot(
     y = index$middle[6],
     label = index$dynasty[6],
     family = "Inknut Antiqua",
-    hjust = 0
+    hjust = 0,
+    size = ann_text_size
   ) +
   annotate(
     "rect",
@@ -187,7 +200,8 @@ ggplot(
     y = index$middle[7],
     label = index$dynasty[7],
     family = "Inknut Antiqua",
-    hjust = 0
+    hjust = 0,
+    size = ann_text_size
   ) +
   annotate(
     "rect",
@@ -204,22 +218,31 @@ ggplot(
     y = index$middle[8],
     label = index$dynasty[8],
     family = "Inknut Antiqua",
-    hjust = 0
+    hjust = 0,
+    size = ann_text_size
   ) +
+  geom_text(
+    aes(x = ANNUS_NATALIS, label = NOMINE),
+    hjust = 1.1,
+    family = "Inknut Antiqua Light",
+    size = 4
+    ) +
   labs(x = "Year",
        title = "Roman Emperors: \nRise and Fall",
-       caption = "#TidyTuesday") +
-  theme_solarized(base_size = 10, base_family = "Inknut Antiqua") +
+       caption = "#TidyTuesday | MywellHernan") +
+  theme_solarized(base_size = 16, base_family = "Inknut Antiqua") +
   theme(
-    axis.text.y = element_text(family = "Inknut Antiqua Light", size = 6),
+    axis.text.y = element_blank(),
+    axis.ticks.y = element_blank(),
     axis.title.y = element_blank(),
     panel.grid.minor = element_blank(),
     panel.grid.major.y = element_blank(),
-    plot.title = element_text(size = 14)
+    plot.title = element_text(size = 20),
+    plot.margin = unit(c(0.5,0.5,0.5,0.5), "cm")
   ) +
-  coord_cartesian(xlim = c(-100, 400))
+  coord_cartesian(xlim = c(-118, 400))
 
-ggsave("2019-08-14/emperors.png", width = 5, height = 15)
+ggsave("2019-08-14/emperors.png", width = 9, height = 21)
 
 
 
